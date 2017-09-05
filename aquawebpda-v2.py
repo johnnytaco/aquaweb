c00d016189e890da30587a11de19b34538188f9f
johnnytaco <30409318+johnnytaco@users.noreply.github.com>
2017-09-05 08:54:07 -0500

    PDA-only version of aquaweb
    This has not been merged with EFP3's version yet, all non-PDA code has been stripped out. Will merge eventually!

--- aquawebpda-v2.py
+++ aquawebpda-v2.py
@@ -2,7 +2,7 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 # coding=utf-8
 
-"""Simulates Aqualink PDA remote with a RS485 interface."""
+"""Simulates Aqualink PDA remote with a RS485 interface"""
 
 import string
 import serial
